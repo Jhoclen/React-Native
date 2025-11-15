d689f54d02b83a79605923c2c582896ab9198aaa
Arthur Fernando <arthurfernando18@gmail.com>
2025-11-15 13:43:49 -0300

    adiciona funcionalidade de prioridade de atendimento

--- Apoio_ubs/src/Main/index.js
+++ Apoio_ubs/src/Main/index.js
@@ -6,71 +6,118 @@ import { useState } from "react";
 import { agenda } from "../mocks/agenda";
 import DataModal from "../components/DataModal";
 import Caixa from "../components/CaixaClick";
-import {EscolherDataModal} from "../components/EscolherDataModel";
-export default function Main(){
-     const [servicoEscolhido, setServicoEscolhido] = useState('Vacina'); // ou 'Medição de Pressão/Glicose', etc
+import { EscolherDataModal } from "../components/EscolherDataModel";
+import PrioridadeModal from "../components/PrioridadeModal";
+import CustomModal from "../components/CustomModal";
+import MeusAgendamentos from "../components/MeusAgendamentos";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-  const handleConfirm = (agendamento) => {
-    console.log('Agendamento confirmado:', agendamento);
-    // agendamento = { servico, data, hora, agendamentoId }
-    alert(`Agendado: ${agendamento.servico} em ${agendamento.data} às ${agendamento.hora}`);
-  };
-   const [datavisuModal, setdatavisuModal] = useState(false);
-   const [datemodel, setdatemodel] = useState(false) 
+export default function Main() {
 
-    function RenderModal(){
+    const [servicoEscolhido, setServicoEscolhido] = useState('Vacina'); // ou 'Medição de Pressão/Glicose', etc
+    const [mostrarMeusAgendamentos, setMostrarMeusAgendamentos] = useState(false);
+
+    const handleConfirm = async (agendamento) => {
+        const agendamentoFinal = {
+            ...agendamento,
+            prioridade
+        };
+
+        try {
+            const data = await AsyncStorage.getItem("@agendamentos");
+            const lista = data ? JSON.parse(data) : [];
+            lista.push(agendamentoFinal);
+
+            await AsyncStorage.setItem("@agendamentos", JSON.stringify(lista));
+        } catch (err) {
+            console.log("Erro ao salvar:", err);
+        }
+
+        alert(
+            `Agendado: ${agendamento.servico} em ${agendamento.data} às ${agendamento.hora}\n` +
+            `Prioridade: ${prioridade}`
+        );
+
+        setdatemodel(false);
+    };
+
+    const [datavisuModal, setdatavisuModal] = useState(false);
+    const [datemodel, setdatemodel] = useState(false)
+    const [prioridade, setPrioridade] = useState(null);
+    const [mostrarPrioridadeModal, setMostrarPrioridadeModal] = useState(false);
+
+    function RenderModal() {
         setdatavisuModal(true)
     }
 
-    function handleServiceClick(Id,servico){
-        
+    function handleServiceClick(Id, servico) {
+
         switch (Id) {
             case 1:
-                setdatemodel(true)
                 setServicoEscolhido(servico)
+                setdatavisuModal(false);
+                setMostrarPrioridadeModal(true);
                 break;
             case 2:
-
-                setdatemodel(true)
                 setServicoEscolhido(servico)
+                setdatavisuModal(false);
+                setMostrarPrioridadeModal(true);
                 break;
             case 3:
-    
-                setdatemodel(true)
                 setServicoEscolhido(servico)
+                setdatavisuModal(false);
+                setMostrarPrioridadeModal(true);
                 break;
-                
-             case 4:
-                setdatemodel(true)
+
+            case 4:
                 setServicoEscolhido(servico)
-                break;    
+                setdatavisuModal(false);
+                setMostrarPrioridadeModal(true);
+                break;
             default:
                 console.warn('ID de Serviço não reconhecido:', Id);
         }
     }
 
-    return(
-    <Container>
-        <Header/>
-        <Services
-            onClick ={RenderModal}
-         />
-        <DataModal 
-        agenda = {agenda}
-        visible={datavisuModal}
-        onClose={()=> setdatavisuModal(false)} 
-        hendlerService={handleServiceClick}
-        >
-        </DataModal>
-        <EscolherDataModal
-            servicoSelecionado={servicoEscolhido}
-            onConfirm={handleConfirm}
-            visible={datemodel}
-            onClose={()=> setdatemodel(false)}
-         />
-        
-        
-    </Container>
+    return (
+        <Container>
+            <Header />
+            <Services
+                onClickAgendar={RenderModal}
+                onClickMeusAgendamentos={() => setMostrarMeusAgendamentos(true)}
+            />
+            <DataModal
+                agenda={agenda}
+                visible={datavisuModal}
+                onClose={() => setdatavisuModal(false)}
+                hendlerService={handleServiceClick}
+            >
+            </DataModal>
+
+            <EscolherDataModal
+                servicoSelecionado={servicoEscolhido}
+                onConfirm={handleConfirm}
+                visible={datemodel}
+                onClose={() => setdatemodel(false)}
+            />
+
+            <PrioridadeModal
+                visible={mostrarPrioridadeModal}
+                onClose={() => setMostrarPrioridadeModal(false)}
+                onFinish={(nivelPrioridade) => {
+                    setPrioridade(nivelPrioridade);
+                    setMostrarPrioridadeModal(false);
+                    setdatemodel(true);
+                }}
+            />
+
+            {mostrarMeusAgendamentos && (
+                <CustomModal visible={mostrarMeusAgendamentos} onClose={() => setMostrarMeusAgendamentos(false)}>
+                    <MeusAgendamentos />
+                </CustomModal>
+            )}
+
+        </Container>
 
     );
 
